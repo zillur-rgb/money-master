bcd41155901cd6910454428da81f9a09fda452e3
Mouri <mouriclub@gmail.com>
2022-02-17 15:55:38 +0600

    second-commit

---
+++ money.js
@@ -0,0 +1,37 @@
+const inCome = document.getElementById("income-input");
+
+
+
+const food = document.getElementById("food-input");
+
+
+const rent = document.getElementById("rent-input");
+
+
+const clothes = document.getElementById("clothes-input");
+
+
+const expenses = document.getElementById("total-expense");
+const balance = document.getElementById('balance')
+
+function totalExpenses(a,b,c){
+    let total = parseFloat(a) + parseFloat(b) + parseFloat(c);
+    return total
+}
+
+document
+  .getElementById("calculate-button")
+  .addEventListener("click", function () {
+      const totallyExpense = totalExpenses(food.value,rent.value,clothes.value);
+      expenses.innerText = totallyExpense ;
+      
+      balance.innerText= parseFloat(inCome.value) - totallyExpense;
+    });
+  
+ 
+   
+document.getElementById("save-button").addEventListener("click", function () {
+  console.log("save btn clicked");
+});
+
+
